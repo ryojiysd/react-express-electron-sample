a610b6fb3cb03fde549be34f67ce500b6ec64f03
Ryoji Yoshida <ryoji.ysd@gmail.com>
2019-04-13 20:58:59 +0900

    Add comments

--- routes/kuji.js
+++ routes/kuji.js
@@ -4,11 +4,13 @@ var router = express.Router()
 
 const kuji = ['大吉', '吉', '凶']
 
+// A function to return a result of Omikuji
 const get_kuji = () => {
   const rnd = Math.floor(Math.random() * kuji.length)
   return kuji[rnd]
 }
 
+// GET /api/kuji
 router.get('/', (req, res) => {
   res.json({result: get_kuji()})
 })
