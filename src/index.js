33cf881414d2db1900c6850036efab0829bc60bd
Ryoji Yoshida <ryoji.ysd@gmail.com>
2019-04-13 14:23:02 +0900

    Add an omikuji feature to client side

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import request from 'superagent'
+
+class Kuji extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { result: '' }
+    this.api = this.api.bind(this)
+  }
+
+  api () {
+    request.get('api/kuji').end((err, res) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      const result = res.body.result
+      this.setState({ result: result })
+    })        
+  }
+
+  render () {
+    return (
+      <div>
+        <p>{this.state.result}</p>
+        <button onClick={ e => this.api() }>ひく</button>
+     </div>
+    )
+  }
+}
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+  <Kuji />,
   document.getElementById('root')
 )
